0dc597770ef294a313d1f82c1c67eefec8873d87
Livio Beqiri <liviob@live.com>
2023-08-25 13:11:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,8 @@ random_secret_key = secrets.token_hex(32)
 
 # keys_defining
 app.secret_key = random_secret_key
-api_key = "bGJlcWlyaUB0ZWxxdWVzdGludGwuY29tOkkoTSpjYTY5cEBvKS1FeTAjeHtEKFNVJ2NmYzVpeQ==  "
-
-
-
+# api_key = "bGJlcWlyaUB0ZWxxdWVzdGludGwuY29tOkkoTSpjYTY5cEBvKS1FeTAjeHtEKFNVJ2NmYzVpeQ==  "
+api_key = "bGJlcWlyaUB0ZWxxdWVzdGludGwuY29tOjFfSDctPjB1Um5pQlZZRSc1NTVqZSZyZVxwb350Zw=="
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -26,7 +24,8 @@ def home():
 def index():
     if request.method == 'POST':
         # Your authentication process here (replace with your actual process)
-        authentication_url = "https://api-services.uat.1placedessaisons.com/uatm/v1/idp/oauth2/authorize"
+        #authentication_url = "https://api-services.uat.1placedessaisons.com/uatm/v1/idp/oauth2/authorize"
+        authentication_url = "https://api.eulerhermes.com/v1/idp/oauth2/authorize"
         payload = json.dumps({"apiKey": api_key})
         headers = {'Content-Type': 'application/json', 'Authorization': '1'}
         response = requests.request("POST",
@@ -40,7 +39,8 @@ def index():
         
 
         # Second API Call - Search for Company
-        search_company_url = "https://api-services.uat.1placedessaisons.com/search/uatm-v2/companies/matching"
+        # search_company_url = "https://api-services.uat.1placedessaisons.com/search/uatm-v2/companies/matching"
+        search_company_url = "https://api.allianz-trade.com/search/v2/companies/matching"
         session['token'] = 'Bearer ' + authentication_token
         token = session.get('token')
         headers = {'Content-Type': 'application/json', 'Authorization': token}
@@ -76,7 +76,8 @@ def index():
 #4th API CALL Function
 def search_jobid(JobId, token):
     
-    search_job_url = 'https://api-services.uat.1placedessaisons.com/uatm/riskinfo/v3/jobs/' + JobId
+    # search_job_url = 'https://api-services.uat.1placedessaisons.com/uatm/riskinfo/v3/jobs/' + JobId
+    search_job_url = 'https://api.allianz-trade.com/riskinfo/v3/jobs/' + JobId
     payload = {}
     headers  = {'Content-Type': 'application/json', 'Authorization': token}
     time.sleep(50)
@@ -93,7 +94,8 @@ def search_jobid(JobId, token):
 def cover_request():
     if request.method == 'POST':
         requested_amount = request.form['amount']
-        cover_request_url = "https://api-services.uat.1placedessaisons.com/uatm/riskinfo/v3/covers"
+        # cover_request_url = "https://api-services.uat.1placedessaisons.com/uatm/riskinfo/v3/covers"
+        cover_request_url = "https://api.allianz-trade.com/riskinfo/v3/covers"
         token = session.get('token')
 
         payload = json.dumps({
